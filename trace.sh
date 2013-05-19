a07e8ad85c7da598f8122c776bdeef51879d8eee
李夏驰 <oldmanpushcart@gmail.com>
2013-05-20 01:50:03 +0800

    create trace.sh file
    trace ksh

---
+++ trace.sh
@@ -0,0 +1,15 @@
+#!/bin/ksh
+
+typeset top=${1:-10}
+typeset pid=${2:-$(pgrep -u $USER java)}
+typeset tmp_file=/tmp/java_$pid_$$.trace
+
+$JAVA_HOME/bin/jstack $pid > $tmp_file
+ps H -eo user,pid,ppid,tid,time,%cpu --sort=%cpu|tail -$top|awk '$2==pid{print $4"\t"$6}' pid=$pid|while read line;
+do
+  typeset nid="0x"$(echo "$line"|awk '{print $1}'|xargs -I{} echo "obase=16;{}"|bc|tr 'A-Z' 'a-z')
+	typeset cpu=$(echo "$line"|awk '{print $2}')
+	cat $tmp_file|awk '/nid='"$nid"'/,/^$/{print $0"\t"(isF++?"":"cpu="'"$cpu"'"%");}'
+done;
+
+rm -f $tmp_file
