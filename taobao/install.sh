a2a56b1bcd98fac9c0c168b713501a478cb644f4
vlinux <oldmanpushcart@gmail.com>
2013-10-07 19:59:21 +0800

    write a install

---
+++ taobao/install.sh
@@ -0,0 +1,7 @@
+#!/bin/ksh
+
+rm -f xssh
+rm -f xssh.ex
+
+wget "https://raw.github.com/oldmanpushcart/unixtools/master/taobao/xssh.ex"
+wget "https://raw.github.com/oldmanpushcart/unixtools/master/taobao/xssh"
