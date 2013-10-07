a9068185b5e6a8ec33389bae9f37d2525bbb0476
vlinux <oldmanpushcart@gmail.com>
2013-10-07 20:07:49 +0800

    init

--- taobao/install.sh
+++ taobao/install.sh
@@ -1,7 +1,11 @@
 #!/bin/ksh
 
+# cleanup
 rm -f xssh
 rm -f xssh.ex
+rm -f lchosts
 
+# download
 curl -s "https://raw.github.com/oldmanpushcart/unixtools/master/taobao/xssh.ex"|iconv -f UTF-8 -t GBK > xssh.ex
 curl -s "https://raw.github.com/oldmanpushcart/unixtools/master/taobao/xssh"|iconv -f UTF-8 -t GBK > xssh
+curl -s "https://raw.github.com/oldmanpushcart/unixtools/master/taobao/lchosts"|iconv -f UTF-8 -t GBK > lchosts
