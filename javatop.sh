b39c5352b6de8bfaf397f410bb5cc4b906f4756f
李夏驰 <oldmanpushcart@gmail.com>
2014-01-16 20:41:58 +0800

    Update javatop.sh

--- javatop.sh
+++ javatop.sh
@@ -1,23 +1,22 @@
 #!/bin/ksh
 
-# write by	: oldmanpushcart@gmail.com
-# date		: 2013-06-16
-# version	: 0.06
-
-#!/bin/ksh
+# write by    : oldmanpushcart@gmail.com
+# date        : 2014-01-16
+# version     : 0.07
 
 typeset top=${1:-10}
 typeset pid=${2:-$(pgrep -u $USER java)}
-typeset tmp_file=/tmp/java_$pid_$$.trace
+typeset tmp_file=/tmp/java_${pid}_$$.trace
 
 $JAVA_HOME/bin/jstack $pid > $tmp_file
-ps H -eo user,pid,ppid,tid,time,%cpu --sort=%cpu|tail -$top|awk '$2==pid{print $4"\t"$6}' pid=$pid|while read line;
+ps H -eo user,pid,ppid,tid,time,%cpu --sort=%cpu --no-headers\
+	| tail -$top\
+	| awk -v "pid=$pid" '$2==pid{print $4"\t"$6}'\
+	| while read line;
 do
-	typeset nid="0x"$(echo "$line"|awk '{print $1}'|xargs -I{} echo "obase=16;{}"|bc|tr 'A-Z' 'a-z')
-	typeset cpu=$(echo "$line"|awk '{print $2}')
-	cat $tmp_file|awk '/nid='"$nid"'/,/^$/{print $0"\t"(isF++?"":"cpu="'"$cpu"'"%");}'
-done;
+	typeset nid=$(echo "$line"|awk '{printf("0x%x",$1)}')
+        typeset cpu=$(echo "$line"|awk '{print $2}')
+        awk -v "cpu=$cpu" '/nid='"$nid"'/,/^$/{print $0"\t"(isF++?"":"cpu="cpu"%");}' $tmp_file
+done
 
 rm -f $tmp_file
-
-
